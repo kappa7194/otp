8e6cf8204e4e8866418f3d1d7dc01a8c052a909c
Albireo <kappa7194@hotmail.it>
2014-03-02 01:39:30 +0100

    Add console application project

---
+++ Albireo.Otp.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Albireo.Otp.ConsoleApplication
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "One-Time Password Generator";
+        }
+    }
+}
