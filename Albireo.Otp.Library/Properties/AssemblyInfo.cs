e9bd0d36ed732fb8a4d5f3f338e8c02abd80e600
Albireo <kappa7194@hotmail.it>
2014-03-02 01:38:06 +0100

    Add library project

---
+++ Albireo.Otp.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Albireo.Totp.Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Albireo.Totp.Library")]
+[assembly: AssemblyCopyright("Copyright © Albireo 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9bedccab-dfe2-48c2-8a6c-ef9443bc730a")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
