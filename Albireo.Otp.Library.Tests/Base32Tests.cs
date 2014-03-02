aa9905a3c46c9c293541886c321f2db8102b5217
Albireo <kappa7194@hotmail.it>
2014-03-02 01:39:49 +0100

    Add base 32 encoder and decoder

---
+++ Albireo.Otp.Library.Tests/Base32Tests.cs
@@ -0,0 +1,102 @@
+﻿namespace Albireo.Otp.Library.Tests
+{
+    using System.Text;
+    using Xunit;
+
+    public class Base32Tests
+    {
+        public class ToStringTests : Base32Tests
+        {
+            [Fact]
+            public void Vector1()
+            {
+                Assert.Equal(string.Empty, Base32.ToString(StringToBytes(string.Empty)));
+            }
+
+            [Fact]
+            public void Vector2()
+            {
+                Assert.Equal("MY======", Base32.ToString(StringToBytes("f")));
+            }
+
+            [Fact]
+            public void Vector3()
+            {
+                Assert.Equal("MZXQ====", Base32.ToString(StringToBytes("fo")));
+            }
+
+            [Fact]
+            public void Vector4()
+            {
+                Assert.Equal("MZXW6===", Base32.ToString(StringToBytes("foo")));
+            }
+
+            [Fact]
+            public void Vector5()
+            {
+                Assert.Equal("MZXW6YQ=", Base32.ToString(StringToBytes("foob")));
+            }
+
+            [Fact]
+            public void Vector6()
+            {
+                Assert.Equal("MZXW6YTB", Base32.ToString(StringToBytes("fooba")));
+            }
+
+            [Fact]
+            public void Vector7()
+            {
+                Assert.Equal("MZXW6YTBOI======", Base32.ToString(StringToBytes("foobar")));
+            }
+        }
+        public class ToBytesTests : Base32Tests
+        {
+            [Fact]
+            public void Vector1()
+            {
+                Assert.Equal(StringToBytes(string.Empty), Base32.ToBytes(string.Empty));
+            }
+
+            [Fact]
+            public void Vector2()
+            {
+                Assert.Equal(StringToBytes("f"), Base32.ToBytes("MY======"));
+            }
+
+            [Fact]
+            public void Vector3()
+            {
+                Assert.Equal(StringToBytes("fo"), Base32.ToBytes("MZXQ===="));
+            }
+
+            [Fact]
+            public void Vector4()
+            {
+                Assert.Equal(StringToBytes("foo"), Base32.ToBytes("MZXW6==="));
+            }
+
+            [Fact]
+            public void Vector5()
+            {
+                Assert.Equal(StringToBytes("foob"), Base32.ToBytes("MZXW6YQ="));
+            }
+
+            [Fact]
+            public void Vector6()
+            {
+                Assert.Equal(StringToBytes("fooba"), Base32.ToBytes("MZXW6YTB"));
+            }
+
+            [Fact]
+            public void Vector7()
+            {
+                Assert.Equal(StringToBytes("foobar"), Base32.ToBytes("MZXW6YTBOI======"));
+            }
+        }
+
+        private byte[] StringToBytes(string s)
+        {
+            return Encoding.UTF8.GetBytes(s);
+        }
+    }
+}
---
+++ Albireo.Otp.Library/Base32.cs
@@ -0,0 +1,90 @@
+﻿namespace Albireo.Otp.Library
+{
+    using System;
+    using System.Linq;
+
+    // Adapted from http://stackoverflow.com/a/7135008
+    // This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+    // To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
+    // or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+    public static class Base32
+    {
+        public static byte[] ToBytes(string input)
+        {
+            if (input == null) throw new ArgumentNullException("input");
+            if (input.Length == 0) return new byte[0];
+            input = input.TrimEnd('=');
+            var byteCount = input.Length * 5 / 8;
+            var result = new byte[byteCount];
+            byte currentByte = 0, bitsRemaining = 8;
+            var arrayIndex = 0;
+            foreach (var value in input.Select(CharToValue))
+            {
+                int mask;
+                if (bitsRemaining > 5)
+                {
+                    mask = value << (bitsRemaining - 5);
+                    currentByte = (byte) (currentByte | mask);
+                    bitsRemaining -= 5;
+                }
+                else
+                {
+                    mask = value >> (5 - bitsRemaining);
+                    currentByte = (byte) (currentByte | mask);
+                    result[arrayIndex++] = currentByte;
+                    unchecked
+                    {
+                        currentByte = (byte)(value << (3 + bitsRemaining));
+                    }
+                    bitsRemaining += 3;
+                }
+            }
+            if (arrayIndex != byteCount) result[arrayIndex] = currentByte;
+            return result;
+        }
+
+        public static string ToString(byte[] input)
+        {
+            if (input == null) throw new ArgumentNullException("input");
+            if (input.Length == 0) return string.Empty;
+            var charCount = (int) Math.Ceiling(input.Length / 5d) * 8;
+            var result = new char[charCount];
+            byte nextChar = 0, bitsRemaining = 5;
+            var arrayIndex = 0;
+            foreach (var b in input)
+            {
+                nextChar = (byte) (nextChar | (b >> (8 - bitsRemaining)));
+                result[arrayIndex++] = ValueToChar(nextChar);
+                if (bitsRemaining < 4)
+                {
+                    nextChar = (byte) ((b >> (3 - bitsRemaining)) & 31);
+                    result[arrayIndex++] = ValueToChar(nextChar);
+                    bitsRemaining += 5;
+                }
+                bitsRemaining -= 3;
+                nextChar = (byte) ((b << bitsRemaining) & 31);
+            }
+            if (arrayIndex == charCount) return new string(result);
+            result[arrayIndex++] = ValueToChar(nextChar);
+            while (arrayIndex != charCount) result[arrayIndex++] = '=';
+            return new string(result);
+        }
+
+        private static int CharToValue(char c)
+        {
+            var value = (int) c;
+            if (value < 91 && value > 64) return value - 65;
+            if (value < 56 && value > 49) return value - 24;
+            if (value < 123 && value > 96) return value - 97;
+            throw new ArgumentException("Character is not a Base32 character.", "c");
+        }
+
+        private static char ValueToChar(byte b)
+        {
+            if (b < 26) return (char) (b + 65);
+            if (b < 32) return (char) (b + 24);
+            throw new ArgumentException("Byte is not a value Base32 value.", "b");
+        }
+
+    }
+}
