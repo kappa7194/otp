952b21edb673acb9922d272b2eca5b7caa3b33bd
Albireo <kappa7194@hotmail.it>
2014-03-10 23:03:47 +0100

    Implement encoding

--- Albireo.Otp.Library/Base32.cs
+++ Albireo.Otp.Library/Base32.cs
@@ -1,12 +1,31 @@
 ﻿namespace Albireo.Otp.Library
 {
     using System;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using System.Text;
 
     internal static class Base32
     {
+        private static readonly char[] Alphabet =
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', '2', '3', '4', '5', '6', '7'
+        };
+
         internal static string Encode(byte[] input)
         {
-            throw new NotImplementedException();
+            Contract.Requires<ArgumentException>(input != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+            if (input.Length == 0) return string.Empty;
+            var bits = input.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')).Aggregate((x, y) => x + y);
+            if (bits.Length % 5 != 0) bits += string.Concat(Enumerable.Repeat('0', 5 - bits.Length % 5));
+            var output = new StringBuilder(bits.Length / 5);
+            for (int i = 0, j = bits.Length / 5; i < j; i++) output.Append(Alphabet[Convert.ToInt32(bits.Substring(5 * i, 5), 2)]);
+            while (output.Length % 8 != 0) output.Append('=');
+            return output.ToString();
         }
 
         internal static byte[] Decode(string input)
