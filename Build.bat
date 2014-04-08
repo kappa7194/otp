951ebed6ac2952e9610e28b4228f86db0cfd2c29
Albireo <kappa7194@hotmail.it>
2014-04-08 22:24:07 +0200

    Fix build script

--- Build.bat
+++ Build.bat
@@ -1,9 +1,7 @@
-PUSHD otp
 git reset --hard
 git clean -d -f -x
 NuGet.exe Restore Albireo.Otp.sln
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Albireo.Otp.sln /property:Configuration=Debug /maxcpucount /nodeReuse:false
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Albireo.Otp.sln /property:Configuration=Release /maxcpucount /nodeReuse:false
 NuGet.exe Pack Albireo.Otp\Albireo.Otp.csproj -Prop Configuration=Release -Symbols
-POPD
 PAUSE
